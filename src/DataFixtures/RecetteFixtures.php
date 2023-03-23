f05693d787649e40c733df915621541536a832f7
ckempee <kempinairecamille@gmail.com>
2023-03-23 15:11:32 +0100

    recette/ingredients fixtures

---
+++ src/DataFixtures/RecetteFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Recette;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class RecetteFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        
+            $recette = new Recette();
+            // créer un hydrate
+            $recette->setTitre("pizza" );
+            $recette->setDureePreparation("25min" );
+            $recette->setTempsCuisson("15min" );
+            $recette->setNbPersonne("2" );
+            $recette->setPhoto("2.png" );
+            $recette->setDescription("Description, description, description" );
+
+            $manager->persist($recette);
+           
+           
+          
+        $manager->flush();
+    }
+}
