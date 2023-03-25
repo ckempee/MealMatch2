18b4aba3e7d39199539b5f9e05f7df012ab4c4ce
ckempee <kempinairecamille@gmail.com>
2023-03-25 16:41:02 +0100

    paginator mesrecettes

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -3,19 +3,30 @@
 namespace App\Controller;
 
 use App\Repository\RecetteRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
 
 class RecetteController extends AbstractController
 {
     //l'index va me permettre de voir mes recettes
     //récuperer le user!!!! 
     #[Route('/recette', name: 'recette.index')]
-    public function index(RecetteRepository $repository): Response
+    public function index(RecetteRepository $repository,PaginatorInterface $paginator,Request $request): Response
     {
         //je récupère toutes les recettes
-        $recettes=$repository->findAll();
+        $recettes=$paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            5
+        );
         
         //je les envois à la vue
         return $this->render('recette/index.html.twig', [
