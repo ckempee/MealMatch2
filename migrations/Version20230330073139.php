73444ba37272e6ddd217b45d46979922bb1c717b
Camille Kempinaire <c.kempinaire@interface3.be>
2023-03-30 09:58:37 +0200

    rajout categorie à nouvelle recette

--- migrations/Version20230330073139.php
+++ migrations/Version20230330073139.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230329115504 extends AbstractMigration
+final class Version20230330073139 extends AbstractMigration
 {
     public function getDescription(): string
     {
